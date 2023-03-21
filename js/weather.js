af6a2437caf4a04f5950572dde3208daa0615517
dothur <craz1000@naver.com>
2023-03-21 23:02:59 +0900

    Practice CSS (BootStrap)

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,22 @@
 // use Openweathermap.org
 const API_KEY = "cd89d16e96912bef46368bd76ede6aaa";
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;   // 위도
-    const lon = position.coords.longitude;  // 경도
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}$units=metric`;
-    fetch(url) // url 호출
-        .then((Response) => Response.json())
-        .then((data) => {
-            const weather = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weather span:last-child");
-            city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-        });
+function onGeoOk(position) {
+  const lat = position.coords.latitude; // 위도
+  const lon = position.coords.longitude; // 경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}&units=metric`;
+  fetch(url) // url 호출
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${Math.round(
+        data.main.temp
+      )}℃`;
+    });
 }
-function onGeoError(){
-    alert("Can't find you. No weather for you.");
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
