e25e3de86340bb529814160ef974096ad2165b16
dothur <craz1000@naver.com>
2023-03-22 02:17:19 +0900

    practice css

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,13 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("span#clock");
 
-function getClock(){
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date. getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours} 시 ${minutes} 분 ${seconds} 초`;
 }
 //padstart -> string 앞줄에 0을 추가함 위에선 길이가 2가 아니면 앞에 0이 나옴
 
 getClock();
-setInterval(getClock, 1000)
+setInterval(getClock, 1000);
--- js/weather.js
+++ js/weather.js
@@ -4,16 +4,19 @@ const API_KEY = "cd89d16e96912bef46368bd76ede6aaa";
 function onGeoOk(position) {
   const lat = position.coords.latitude; // 위도
   const lon = position.coords.longitude; // 경도
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
   fetch(url) // url 호출
     .then((Response) => Response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      const weather = document.querySelector("#weatherSpan");
+      const city = document.querySelector("#citySpan");
+      const weatherIconImg = document.querySelector("#weatherIcon");
+      const weatherIcon = data.weather[0].icon;
+      const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${Math.round(
-        data.main.temp
-      )}℃`;
+      weather.innerText = `${Math.round(data.main.temp)}℃`;
+      weatherIconImg.src = weatherIconUrl;
     });
 }
 function onGeoError() {
