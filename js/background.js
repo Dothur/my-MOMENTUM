a261e17cbb7d19ece0f5e77cad7b8dba0bf384aa
dothur <craz1000@naver.com>
2023-03-26 23:56:03 +0900

    background img 수정

--- js/background.js
+++ js/background.js
@@ -1,12 +1,6 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
-
-const chooseImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-// html에 img element를 추가
-
-bgImage.src = `img/${chooseImage}`;
-
-document.body.appendChild(bgImage);
-//append -> 가장 뒤에 추가
-//prepend -> 가장 앞에 추가
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center top";
