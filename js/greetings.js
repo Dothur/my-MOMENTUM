0e4373953b2fb237df516d2faa73e75a0ef67d20
dothur <craz1000@naver.com>
2023-04-02 23:34:11 +0900

    test

--- js/greetings.js
+++ js/greetings.js
@@ -6,26 +6,26 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault(); // 브라우저 기본동작을 막아줌
-    loginForm.classList.add(HIDDEN_CLASSNAME); // hidden class로 추가
-    const username =  loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    // greeting.innerText = "Hello " + username;
-    paintGreetings(username)
+  event.preventDefault(); // 브라우저 기본동작을 막아줌
+  loginForm.classList.add(HIDDEN_CLASSNAME); // hidden class로 추가
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  // greeting.innerText = "Hello " + username;
+  paintGreetings(username);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
-    // show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    // show the greetings
-    paintGreetings(savedUsername);
+  // show the greetings
+  paintGreetings(savedUsername);
 }
