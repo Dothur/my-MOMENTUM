6f70bd15970943d9af6376c8b601a55341b4f30f
dothur <craz1000@naver.com>
2023-04-02 23:04:11 +0900

    Change backgroundImage

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 document.body.style.backgroundImage = `url('img/${chosenImage}')`;
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
-document.body.style.backgroundPosition = "center top";
+document.body.style.backgroundPosition = "center";
